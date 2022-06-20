8a8a814150a1fb8be4033ba0d546fdb749dbf456
mirjanazidar <106159508+mirjanazidar@users.noreply.github.com>
2022-06-20 08:03:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log ("Welcome to my portfolio");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 640){
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
